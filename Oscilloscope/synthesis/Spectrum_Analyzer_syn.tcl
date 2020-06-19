6177d0903ffb62d53e7c5909b838388cbf7cae04
PKazm <paulkazmierski@gmail.com>
2020-06-18 23:43:04 -0400

    ignore libero folders

--- Oscilloscope/synthesis/Spectrum_Analyzer_syn.tcl
+++
@@ -1,3 +0,0 @@
-project -load "C:/Users/Phoenix136/Dropbox/FPGA/Microsemi/Oscilloscope/synthesis/Spectrum_Analyzer_syn.prj"
-project -run
-project -save
--- Oscilloscope/synthesis/synlog.tcl
+++
@@ -1,1 +0,0 @@
-run_tcl -fg Spectrum_Analyzer_syn.tcl
